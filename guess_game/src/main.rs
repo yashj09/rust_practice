9af4656b82b15e9b5526bee9e442af5470c69126
Yash Jain <118172073+yashj09@users.noreply.github.com>
2026-05-04 03:13:30 +0530

    user input

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the Number");
+
+    println!("Please input a number");
+
+    let mut num= String::new();
+
+    io::stdin().read_line(&mut num).expect("input needed");
+
+    println!("You Guessed: {num}")
 }
