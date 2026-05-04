e4d28a504d0ebb3ef6c23df68733ffa54652c8a2
Yash Jain <118172073+yashj09@users.noreply.github.com>
2026-05-04 23:42:03 +0530

    added rand & cmp

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,13 +1,24 @@
 use std::io;
-
+use rand:: RngExt;
+use std::cmp::Ordering;
 fn main() {
     println!("Guess the Number");
+let secret= rand::rng().random_range(1..=100);
+println!("the secret number is {secret}");
+
 
     println!("Please input a number");
 
     let mut num= String::new();
 
     io::stdin().read_line(&mut num).expect("input needed");
+    let num:u32 = num.trim().parse().expect("need to be a number");
 
-    println!("You Guessed: {num}")
+    println!("You Guessed: {num}");
+
+    match num.cmp(&secret)  {
+        Ordering::Less =>println!("number too small"),
+        Ordering::Greater=> println!("number too big"),
+    Ordering::Equal => println!("You Win!")
+    }
 }
