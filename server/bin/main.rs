2429c417bc41fff808160f4898f0a18e5e1cfd3f
Yash Jain <118172073+yashj09@users.noreply.github.com>
2026-07-16 00:08:13 +0530

    multi thread server done

---
+++ server/bin/main.rs
@@ -0,0 +1,43 @@
+use server::ThreadPool;
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| handle_connection(stream));
+        // handle_connection(stream);
+        println!("connection estaliblised");
+    }
+}
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 ok", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 ok", "index.html")
+    } else {
+        ("HTTP/1.1 200 ok", "404.html")
+    };
+    let status_line = "HTTP/1.1 404 NOT FOUND";
+    let contents = fs::read_to_string(filename).unwrap();
+    let res = format!(
+        "{}\r\nContent-Length: {} \r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write(res.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
